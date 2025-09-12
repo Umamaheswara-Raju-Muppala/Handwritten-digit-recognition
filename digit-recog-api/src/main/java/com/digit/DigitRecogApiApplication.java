ab5dec20922be54d87aa1e685f995138c58e7782
Umamaheswara-Raju-Muppala <mahidarling678@gmail.com>
2025-09-12 19:55:47 +0530

    setup Spring Boot project with starter and ML dependencies

---
+++ digit-recog-api/src/main/java/com/digit/DigitRecogApiApplication.java
@@ -0,0 +1,13 @@
+package com.digit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitRecogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitRecogApiApplication.class, args);
+	}
+
+}
